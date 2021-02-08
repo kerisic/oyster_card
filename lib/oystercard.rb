da0a21f116b35c8550a9310100061a547c154c6e
Christine Chee <christinechee88@hotmail.com>
2021-02-08 15:49:39 +0000

    Add max balance to top up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,6 @@
 class OysterCard
 DEFAULTBALANCE = 0
+MAXBALANCE = 90
 
 attr_reader :balance
 
@@ -8,7 +9,8 @@ attr_reader :balance
   end
 
   def top_up(top_up_value)
-    @balance = @balance + top_up_value 
+    @balance = @balance + top_up_value
+    fail "Your balance cannot be over £#{MAXBALANCE}" if @balance > MAXBALANCE
   end
 
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -5,7 +5,10 @@ describe OysterCard do
     expect(subject.balance).to eq(0)
   end
  it "tops up with the top up value as the argument" do
-   subject.top_up(200)
-   expect(subject.balance).to eq 200
+   subject.top_up(50)
+   expect(subject.balance).to eq 50
+ end
+ it "raises an error when top up value exceeds maximum value" do
+   expect { subject.top_up(91) }.to raise_error "Your balance cannot be over £90"
  end
 end
